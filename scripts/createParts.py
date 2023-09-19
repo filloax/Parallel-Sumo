b8baecdb731b1c2c45769c6356a21aa25fcdc38c
filloax <filloax@gmail.com>
2023-09-19 03:43:20 +0200

    fully replace cutRoutes.py

--- scripts/createParts.py
+++ scripts/createParts.py
@@ -10,6 +10,7 @@ Adapted by Filippo Lenzi
 import os, sys
 import subprocess
 import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element
 import argparse
 from convertToMetis import main as convert_to_metis
 
@@ -47,6 +48,8 @@ def partition_network(
     # Partition network with METIS
     if use_metis:
         print("Running convertToMetis.py to split graph...")
+        
+        convert_to_metis(net_file, num_threads)
 
         # Read actual partition num from METIS output
         part_num_file = os.path.join(data_folder, "numParts.txt")
@@ -95,56 +98,115 @@ def partition_network(
     processed_routes_path = os.path.join(data_folder, "processed_routes.xml")
     routes_tree.write(processed_routes_path)
 
+    # vehicle_id: depart_time
+    min_depart_times = {}
+    temp_files = []
+
     for i in range(num_threads):
-        charI = str(i)
-        net_part = os.path.abspath(os.path.join(data_folder, f"part{charI}.net.xml"))
-        rou_part = os.path.abspath(os.path.join(data_folder, f"part{charI}.rou.xml"))
-        cfg_part = os.path.abspath(os.path.join(data_folder, f"part{charI}.sumocfg"))
+        _process_partition(
+            i, cfg_dir, cfg_file, net_file,
+            processed_routes_path, net_convert_bin, netconvert_option1,
+            part_bounds, min_depart_times, temp_files,
+            data_folder, use_metis, 
+        )
+        
+    for i in range(num_threads):
+        _postprocess_partition(i, min_depart_times, data_folder)
+    
+    for file in temp_files:
+        os.remove(file)
 
-        if use_metis:
-            netconvert_option2 = os.path.join(data_folder, f"edgesPart{charI}.txt")
-        else:
-            netconvert_option2 = part_bounds[i]
+def _process_partition(
+    part_idx: int, cfg_dir: str, cfg_file: str, net_file: str,
+    processed_routes_path: str,
+    net_convert_bin: str, netconvert_option1: str,
+    part_bounds: dict,
+    min_depart_times: dict, temp_files: list,
+    data_folder: str = "data", use_metis = True
+):
+    net_part = os.path.abspath(os.path.join(data_folder, f"part{part_idx}.net.xml"))
+    interm_rou_part = os.path.abspath(os.path.join(data_folder, f"part{part_idx}.interm.rou.xml"))
+    rou_part = os.path.abspath(os.path.join(data_folder, f"part{part_idx}.rou.xml"))
+    cfg_part = os.path.abspath(os.path.join(data_folder, f"part{part_idx}.sumocfg"))
 
-        # Create partition
-        print(f"Running netConvert for partition {i}...")
-        subprocess.run([
-            net_convert_bin,
-            netconvert_option1, netconvert_option2,
-            "-s", net_file,
-            "-o", net_part
-        ], check=True)
-        print(f"Partition {i} successfully created")
+    if use_metis:
+        netconvert_option2 = os.path.join(data_folder, f"edgesPart{part_idx}.txt")
+    else:
+        netconvert_option2 = part_bounds[part_idx]
 
-        print(f"Running cutRoutes.py to create routes for partition {i}...")
-        cut_routes(cut_routes_options([
-            net_part, processed_routes_path,
-            "--routes-output", rou_part,
-            "--orig-net", net_file,
-            "--disconnected-action", "keep"
-        ]))
+    # Create partition
+    print(f"Running netConvert for partition {part_idx}...")
+    subprocess.run([
+        net_convert_bin,
+        netconvert_option1, netconvert_option2,
+        "-s", net_file,
+        "-o", net_part
+    ], check=True)
+    print(f"Partition {part_idx} successfully created")
 
-        # Create sumo cfg file for partition
-        with open(cfg_file, "r") as source, open(cfg_part, "w") as dest:
-            for line in source:
-                dest.write(line)
+    print(f"Running cutRoutes.py to create routes for partition {part_idx}...")
+    # Put output of cut_routes in temp file, edit it, then save it
+    cut_routes(cut_routes_options([
+        net_part, processed_routes_path,
+        "--routes-output", interm_rou_part,
+        "--orig-net", net_file,
+        "--disconnected-action", "keep"
+    ]))
+    temp_files.append(interm_rou_part)
+    
+    with open(interm_rou_part, 'r', encoding='utf-8') as f:
+        route_part_el: Element = ET.parse(f).getroot()
+        vehicles = route_part_el.findall("vehicle")
+        for vehicle in vehicles:
+            id = vehicle.attrib["id"]
+            depart = float(vehicle.attrib["depart"])
+            if id not in min_depart_times:
+                min_depart_times[id] = depart
+            elif min_depart_times[id] > depart:
+                min_depart_times[id] = depart
 
-        # Set partition net-file and route-files in cfg file
-        cfg_part_tree = ET.parse(cfg_part)
-        cfg_part_el = cfg_part_tree.getroot()
-        input_el = cfg_part_el.find("input")
-        net_file_el = input_el.find("net-file")
-        rou_file_el = input_el.find("route-files")
-        gui_file_el = input_el.find("gui-settings-file")
+    # Create sumo cfg file for partition
+    with open(cfg_file, "r") as source, open(cfg_part, "w") as dest:
+        for line in source:
+            dest.write(line)
 
-        net_file_el.set("value", net_part)
-        rou_file_el.set("value", rou_part)
+    # Set partition net-file and route-files in cfg file
+    cfg_part_tree = ET.parse(cfg_part)
+    cfg_part_el = cfg_part_tree.getroot()
+    input_el = cfg_part_el.find("input")
+    net_file_el = input_el.find("net-file")
+    rou_file_el = input_el.find("route-files")
+    gui_file_el = input_el.find("gui-settings-file")
 
-        if gui_file_el is not None:
-            gui_prev_val = gui_file_el.get("value")
-            gui_file_el.set("value", os.path.abspath(os.path.join(cfg_dir, gui_prev_val)))
+    net_file_el.set("value", net_part)
+    rou_file_el.set("value", rou_part)
 
-        cfg_part_tree.write(cfg_part)
+    if gui_file_el is not None:
+        gui_prev_val = gui_file_el.get("value")
+        gui_file_el.set("value", os.path.abspath(os.path.join(cfg_dir, gui_prev_val)))
+
+    cfg_part_tree.write(cfg_part)
+
+def _postprocess_partition(
+    part_idx: int, min_depart_times: dict,
+    data_folder: str = "data",
+):
+    interm_rou_part = os.path.abspath(os.path.join(data_folder, f"part{part_idx}.interm.rou.xml"))
+    rou_part = os.path.abspath(os.path.join(data_folder, f"part{part_idx}.rou.xml"))
+    
+    with open(interm_rou_part, 'r', encoding='utf-8') as fr:
+        route_part_tree = ET.parse(fr)
+        route_part_el: Element = route_part_tree.getroot()
+        vehicles = route_part_el.findall("vehicle")
+        remove = []
+        for vehicle in vehicles:
+            id = vehicle.attrib["id"]
+            depart = float(vehicle.attrib["depart"])
+            if depart > min_depart_times[id]:
+                remove.append(vehicle)
+        for el in remove: 
+            route_part_el.remove(el)
+        route_part_tree.write(rou_part)
 
 def main(args):
     net_convert_bin = os.path.join(os.path.join(SUMO_HOME, 'bin', 'netconvert'))
