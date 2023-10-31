11558e846e21aa5557afd31c452774a0b9ee9b61
filloax <filloax@gmail.com>
2023-10-31 13:42:03 +0100

    vehicle by step graph generation

--- scripts/gatherStepVehicles.py
+++ scripts/gatherStepVehicles.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import glob
+from matplotlib import pyplot as plt
 
 file_paths = glob.glob("data/stepVehicles*.csv")
 data = {}
@@ -10,5 +11,11 @@ for file_path in file_paths:
 
 result = pd.DataFrame(data)
 result = result.reindex(sorted(result.columns), axis=1)
+result = result.rename(columns={val: f'p{val}' for val in df.columns})
 result.to_csv("data/allStepVehicles.csv")
 print("Saved data/allStepVehicles.csv")
+
+fig, ax = plt.subplots(figsize=(30, 15))
+result.rolling(window=int(len(result)/40)).mean().plot.line(ax=ax)
+plt.savefig("data/allStepVehicles.png")
+print("Saved data/allStepVehicles.png")
